91e5d1a2618eb96b8ce52dc2206cecb3155fba07
Rananjaya Bandara <rananjayabandara@gmail.com>
2021-09-11 21:43:01 +0530

    add card components as a custom componement

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View,SafeAreaView} from 'react-native';
+// import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import { StyleSheet, Text, View,SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View,SafeAreaView,Image } from 'react-native'
+import tw from 'tailwind-react-native-classnames';
+import NavOptions from '../components/navOptions';
+import uber_logo from '../img/Uber_Logo_Black.png';
+// import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+
 
 const HomeScreen = () => {
     return (
-        <SafeAreaView>
-            <Text>I'm the HomeScreen</Text>
+        <SafeAreaView style={tw`bg-white h-full`}>
+           <View style={tw`p-5`}>
+               <Image style={{width:100,height:100}} source={uber_logo} />
+               <NavOptions/>
+          </View>
         </SafeAreaView>
     )
 }
